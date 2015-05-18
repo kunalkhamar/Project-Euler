b45779a8bb2eb9fc3651a61442b1977981c63434
Kunal Khamar <kkhamar@outlook.com>
2015-05-18 11:06:32 -0400

    add 9

---
+++ p009.java
@@ -0,0 +1,28 @@
+/*
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a^2 + b^2 = c^2
+
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+*/
+
+public class p009 {
+
+	public static void main(String[] args) {
+		boolean found = false;
+		for (int a = 1; a <= 999 && !found; a++) {
+			for (int b = a + 1; b <= 1000 && !found; b++) {
+				int c = 1000 - a - b;
+				if (c < 1) continue;
+				
+				if (a * a + b * b == c * c) {
+					System.out.println(a * b * c);
+					found = true;
+				}
+			}
+		}
+	}
+
+}
